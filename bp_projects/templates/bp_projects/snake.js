dd2c541be2a94d8bdb3147930a38c7a13d77fa18
Srihita Kotturi <srihita.kotturi@gmail.com>
2022-10-26 12:08:38 -0700

    1

--- bp_projects/templates/bp_projects/snake.js
+++ bp_projects/templates/bp_projects/snake.js
@@ -10,6 +10,12 @@ var context;
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
+var velocityX = 0;
+var velocityY = 0;
+
+// Food
+var foodX;
+var foodY;
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -17,7 +23,10 @@ window.onload = function() {
     board.width = cols * blockSize;
     context = board.getContext("2d"); // this is used for the drawing on the board
 
-    update();
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    //update();
+    setInterval(update, 1000/10); // every 100 milliseconds it is going to run
 }
 
 function update() {
@@ -25,5 +34,35 @@ function update() {
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle="lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle="red";
+    context.fillRect(foodX, foodY, blockSize, blockSize); 
+}
+
+function changeDirection() {
+    if (e.code == "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    if (e.code == "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
